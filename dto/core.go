c9c58b880d80a5f4289d9a46ba368990d3b7293a
Olakotan Daniel <officialhorlakz@gmail.com>
2025-08-08 19:25:30 +0100

    fix(dto): add TransactionDto struct for transaction details

--- dto/core.go
+++ dto/core.go
@@ -8,3 +8,14 @@ type WalletDetailsDto struct {
 	Balance       decimal.Decimal `json:"balance"`
 	AccountNumber string          `json:"account_number"`
 }
+
+type TransactionDto struct {
+	Reference   string          `json:"reference"`
+	Type        string          `json:"type"`
+	Status      string          `json:"status"`
+	Amount      decimal.Decimal `json:"amount"`
+	Currency    string          `json:"currency"`
+	Description string          `json:"description"`
+	CreatedAt   string          `json:"created_at"`
+	UpdatedAt   string          `json:"updated_at"`
+}
