6e7bb652af4ccd24880dd2fe56e3aedd0ebc3316
Olakotan Daniel <officialhorlakz@gmail.com>
2025-08-09 05:03:55 +0100

    feat(wallet): refactor FundWallet, WithdrawFromWallet, and TransferFunds methods to use TxHelper for transaction management

--- service/wallet_service.go
+++ service/wallet_service.go
@@ -5,9 +5,11 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/horlakz/wallet-sync.api/dto"
+	"github.com/horlakz/wallet-sync.api/lib/database"
 	"github.com/horlakz/wallet-sync.api/model"
 	core_repository "github.com/horlakz/wallet-sync.api/repository/core"
 	"github.com/shopspring/decimal"
+	"gorm.io/gorm"
 )
 
 type WalletServiceInterface interface {
@@ -21,106 +23,125 @@ type walletService struct {
 	accountRepo     core_repository.AccountRepository
 	transactionRepo core_repository.TransactionRepository
 	ledgerEntryRepo core_repository.LedgerEntryRepository
+	db              database.DatabaseInterface
 }
 
 func NewWalletService(
 	accountRepo core_repository.AccountRepository,
 	transactionRepo core_repository.TransactionRepository,
 	ledgerEntryRepo core_repository.LedgerEntryRepository,
+	db database.DatabaseInterface,
 ) WalletServiceInterface {
 	return &walletService{
 		accountRepo:     accountRepo,
 		transactionRepo: transactionRepo,
 		ledgerEntryRepo: ledgerEntryRepo,
+		db:              db,
 	}
 }
+
 func (s *walletService) FundWallet(userID uuid.UUID, amount decimal.Decimal) error { // use a transaction and rollback if any step fails
-	account, err := s.accountRepo.GetAccountByUserID(userID)
-	if err != nil {
-		return err
-	}
+	return s.TxHelper(func(
+		accountRepo core_repository.AccountRepository,
+		transactionRepo core_repository.TransactionRepository,
+		ledgerEntryRepo core_repository.LedgerEntryRepository,
+	) error {
 
-	// Update account balance
-	if err := s.accountRepo.UpdateAccountBalance(userID, amount); err != nil {
-		return err
-	}
+		account, err := accountRepo.GetAccountByUserID(userID)
+		if err != nil {
+			return err
+		}
 
-	// Create a transaction record
-	transaction := model.Transaction{
-		UserID:      *account.UserID,
-		Type:        model.Credit,
-		Status:      model.TransactionCompleted,
-		Amount:      amount,
-		Currency:    "NGN",
-		Description: "Wallet funding",
-	}
+		// Update account balance
+		if err := accountRepo.UpdateAccountBalance(userID, amount); err != nil {
+			return err
+		}
 
-	if err := s.transactionRepo.CreateTransaction(&transaction); err != nil {
-		return err
-	}
+		// Create a transaction record
+		transaction := model.Transaction{
+			UserID:      *account.UserID,
+			Type:        model.Credit,
+			Status:      model.TransactionCompleted,
+			Amount:      amount,
+			Currency:    "NGN",
+			Description: "Wallet funding",
+		}
 
-	// Create a ledger entry
-	ledgerEntry := &model.LedgerEntry{
-		UserID:        userID,
-		AccountID:     account.ID,
-		TransactionID: transaction.ID,
-		EntryType:     "credit",
-		Amount:        amount,
-		Description:   "Wallet funding",
-	}
+		if err := transactionRepo.CreateTransaction(&transaction); err != nil {
+			return err
+		}
 
-	if err := s.ledgerEntryRepo.CreateLedgerEntry(ledgerEntry); err != nil {
-		return err
-	}
+		// Create a ledger entry
+		ledgerEntry := &model.LedgerEntry{
+			UserID:        userID,
+			AccountID:     account.ID,
+			TransactionID: transaction.ID,
+			EntryType:     "credit",
+			Amount:        amount,
+			Description:   "Wallet funding",
+		}
 
-	return nil
+		if err := ledgerEntryRepo.CreateLedgerEntry(ledgerEntry); err != nil {
+			return err
+		}
+
+		return nil
+	})
 }
 
 func (s *walletService) WithdrawFromWallet(userID uuid.UUID, amount decimal.Decimal) error {
-	account, err := s.accountRepo.GetAccountByUserID(userID)
-	if err != nil {
-		return err
-	}
+	return s.TxHelper(func(
+		accountRepo core_repository.AccountRepository,
+		transactionRepo core_repository.TransactionRepository,
+		ledgerEntryRepo core_repository.LedgerEntryRepository,
+	) error {
 
-	// Check if the account has sufficient balance
-	if account.Balance.LessThan(amount) {
-		return errors.New("insufficient balance")
-	}
+		account, err := accountRepo.GetAccountByUserID(userID)
+		if err != nil {
+			return err
+		}
 
-	// Update account balance
-	if err := s.accountRepo.UpdateAccountBalance(userID, amount.Neg()); err != nil {
-		return err
-	}
+		// Check if the account has sufficient balance
+		if account.Balance.LessThan(amount) {
+			return errors.New("insufficient balance")
+		}
 
-	// Create a transaction record
-	transaction := &model.Transaction{
-		UserID:      *account.UserID,
-		Type:        model.Debit,
-		Status:      model.TransactionCompleted,
-		Amount:      amount,
-		Currency:    "NGN",
-		Description: "Wallet withdrawal",
-	}
+		// Update account balance
+		if err := accountRepo.UpdateAccountBalance(userID, amount.Neg()); err != nil {
+			return err
+		}
 
-	if err := s.transactionRepo.CreateTransaction(transaction); err != nil {
-		return err
-	}
+		// Create a transaction record
+		transaction := &model.Transaction{
+			UserID:      *account.UserID,
+			Type:        model.Debit,
+			Status:      model.TransactionCompleted,
+			Amount:      amount,
+			Currency:    "NGN",
+			Description: "Wallet withdrawal",
+		}
 
-	// Create a ledger entry
-	ledgerEntry := &model.LedgerEntry{
-		UserID:        userID,
-		AccountID:     account.ID,
-		TransactionID: transaction.ID,
-		EntryType:     "debit",
-		Amount:        amount,
-		Description:   "Wallet withdrawal",
-	}
+		if err := transactionRepo.CreateTransaction(transaction); err != nil {
+			return err
+		}
 
-	if err := s.ledgerEntryRepo.CreateLedgerEntry(ledgerEntry); err != nil {
-		return err
-	}
+		// Create a ledger entry
+		ledgerEntry := &model.LedgerEntry{
+			UserID:        userID,
+			AccountID:     account.ID,
+			TransactionID: transaction.ID,
+			EntryType:     "debit",
+			Amount:        amount,
+			Description:   "Wallet withdrawal",
+		}
 
-	return nil
+		if err := ledgerEntryRepo.CreateLedgerEntry(ledgerEntry); err != nil {
+			return err
+		}
+
+		return nil
+	})
+
 }
 
 func (s *walletService) GetWalletDetails(userID uuid.UUID) (dto.WalletDetailsDto, error) {
@@ -136,89 +157,111 @@ func (s *walletService) GetWalletDetails(userID uuid.UUID) (dto.WalletDetailsDto
 }
 
 func (s *walletService) TransferFunds(fromUserID uuid.UUID, toAccountNumber string, amount decimal.Decimal) error {
-	fromAccount, err := s.accountRepo.GetWalletAccountByUserID(fromUserID)
-	if err != nil {
-		return err
-	}
 
-	toAccount, err := s.accountRepo.GetAccountByNumber(toAccountNumber)
-	if err != nil {
-		return err
-	}
+	return s.TxHelper(func(
+		accountRepo core_repository.AccountRepository,
+		transactionRepo core_repository.TransactionRepository,
+		ledgerEntryRepo core_repository.LedgerEntryRepository,
+	) error {
 
-	// check if wallet belongs to self
-	if *toAccount.UserID == fromUserID {
-		return errors.New("cannot transfer to your own account")
-	}
+		fromAccount, err := accountRepo.GetWalletAccountByUserID(fromUserID)
+		if err != nil {
+			return err
+		}
 
-	// Check if the from account has sufficient balance
-	if fromAccount.Balance.LessThan(amount) {
-		return errors.New("insufficient balance")
-	}
+		toAccount, err := accountRepo.GetAccountByNumber(toAccountNumber)
+		if err != nil {
+			return err
+		}
 
-	// Update the balances of both accounts
-	if err := s.accountRepo.UpdateAccountBalance(*fromAccount.UserID, amount.Neg()); err != nil {
-		return err
-	}
-	if err := s.accountRepo.UpdateAccountBalance(*toAccount.UserID, amount); err != nil {
-		return err
-	}
+		// check if wallet belongs to self
+		if *toAccount.UserID == fromUserID {
+			return errors.New("cannot transfer to your own account")
+		}
 
-	// Create a transaction record for the sender
-	senderTransaction := &model.Transaction{
-		UserID:      *fromAccount.UserID,
-		Type:        model.Debit,
-		Status:      model.TransactionCompleted,
-		Amount:      amount,
-		Currency:    "NGN",
-		Description: "Transfer to " + toAccount.Number,
-	}
+		// Check if the from account has sufficient balance
+		if fromAccount.Balance.LessThan(amount) {
+			return errors.New("insufficient balance")
+		}
 
-	if err := s.transactionRepo.CreateTransaction(senderTransaction); err != nil {
-		return err
-	}
+		// Update the balances of both accounts
+		if err := accountRepo.UpdateAccountBalance(*fromAccount.UserID, amount.Neg()); err != nil {
+			return err
+		}
+		if err := accountRepo.UpdateAccountBalance(*toAccount.UserID, amount); err != nil {
+			return err
+		}
 
-	// Create a ledger entry for the sender
-	senderLedgerEntry := &model.LedgerEntry{
-		UserID:        *fromAccount.UserID,
-		AccountID:     fromAccount.ID,
-		TransactionID: senderTransaction.ID,
-		EntryType:     "debit",
-		Amount:        amount,
-		Description:   "Transfer to " + toAccount.Number,
-	}
+		// Create a transaction record for the sender
+		senderTransaction := &model.Transaction{
+			UserID:      *fromAccount.UserID,
+			Type:        model.Debit,
+			Status:      model.TransactionCompleted,
+			Amount:      amount,
+			Currency:    "NGN",
+			Description: "Transfer to " + toAccount.Number,
+		}
 
-	if err := s.ledgerEntryRepo.CreateLedgerEntry(senderLedgerEntry); err != nil {
-		return err
-	}
+		if err := transactionRepo.CreateTransaction(senderTransaction); err != nil {
+			return err
+		}
 
-	// Create a transaction record for the receiver
-	receiverTransaction := &model.Transaction{
-		UserID:      *toAccount.UserID,
-		Type:        model.Credit,
-		Status:      model.TransactionCompleted,
-		Amount:      amount,
-		Currency:    "NGN",
-		Description: "Transfer from " + fromAccount.Number,
-	}
+		// Create a ledger entry for the sender
+		senderLedgerEntry := &model.LedgerEntry{
+			UserID:        *fromAccount.UserID,
+			AccountID:     fromAccount.ID,
+			TransactionID: senderTransaction.ID,
+			EntryType:     "debit",
+			Amount:        amount,
+			Description:   "Transfer to " + toAccount.Number,
+		}
 
-	if err := s.transactionRepo.CreateTransaction(receiverTransaction); err != nil {
-		return err
-	}
+		if err := ledgerEntryRepo.CreateLedgerEntry(senderLedgerEntry); err != nil {
+			return err
+		}
 
-	// Create a ledger entry for the receiver
-	receiverLedgerEntry := &model.LedgerEntry{
-		UserID:        *toAccount.UserID,
-		AccountID:     toAccount.ID,
-		TransactionID: receiverTransaction.ID,
-		EntryType:     "credit",
-		Amount:        amount,
-		Description:   "Transfer from " + fromAccount.Number,
-	}
+		// Create a transaction record for the receiver
+		receiverTransaction := &model.Transaction{
+			UserID:      *toAccount.UserID,
+			Type:        model.Credit,
+			Status:      model.TransactionCompleted,
+			Amount:      amount,
+			Currency:    "NGN",
+			Description: "Transfer from " + fromAccount.Number,
+		}
 
-	if err := s.ledgerEntryRepo.CreateLedgerEntry(receiverLedgerEntry); err != nil {
-		return err
-	}
+		if err := transactionRepo.CreateTransaction(receiverTransaction); err != nil {
+			return err
+		}
 
-	return nil
+		// Create a ledger entry for the receiver
+		receiverLedgerEntry := &model.LedgerEntry{
+			UserID:        *toAccount.UserID,
+			AccountID:     toAccount.ID,
+			TransactionID: receiverTransaction.ID,
+			EntryType:     "credit",
+			Amount:        amount,
+			Description:   "Transfer from " + fromAccount.Number,
+		}
+
+		if err := ledgerEntryRepo.CreateLedgerEntry(receiverLedgerEntry); err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
+
+// TxHelper wraps a function in a DB transaction and injects repository instances with the transaction context.
+func (s *walletService) TxHelper(fn func(
+	accountRepo core_repository.AccountRepository,
+	transactionRepo core_repository.TransactionRepository,
+	ledgerEntryRepo core_repository.LedgerEntryRepository,
+) error) error {
+	return s.db.Connection().Transaction(func(tx *gorm.DB) error {
+		accountRepoTx := s.accountRepo.WithTx(tx)
+		transactionRepoTx := s.transactionRepo.WithTx(tx)
+		ledgerEntryRepoTx := s.ledgerEntryRepo.WithTx(tx)
+		return fn(accountRepoTx, transactionRepoTx, ledgerEntryRepoTx)
+	})
 }
